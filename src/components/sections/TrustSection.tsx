d6e74d4a4d03ce82796c6773defee218a2240bf0
michael-wilfert <michael@athlea.ai>
2026-06-08 22:59:58 +0100

    Use provided inverted NFLPA logo

--- src/components/sections/TrustSection.tsx
+++ src/components/sections/TrustSection.tsx
@@ -31,7 +31,7 @@ const clients = [
     name: 'NFLPA',
     src: '/logos/nflpa.png',
     className: 'h-[50px] w-auto',
-    filterClass: '[filter:grayscale(1)_invert(1)_brightness(1.25)_contrast(1.15)]',
+    filterClass: '[filter:grayscale(1)_brightness(1.12)_contrast(1.05)]',
   },
 ]
 
